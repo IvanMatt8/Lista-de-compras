84ca22ed906d792be9f2a939b8d59edeac8f50cd
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-21 14:34:06 -0300

    :pencil: adicionado o import do component AddArea e acrescentado no body do App.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
 import { ListItem } from "./components/ListItem";
+import { AddArea } from "./components/AddArea";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, name: "Pão com batata", done: false },
-    { id: 2, name: "Presunto e Queijo", done: false },
+    { id: 2, name: "Presunto e Queijo", done: true },
   ]);
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Compras </C.Header>
 
-        {/* {***} */}
+        <AddArea />
 
         {list.map((item, index) => (
           <ListItem key={index} item={item} />
