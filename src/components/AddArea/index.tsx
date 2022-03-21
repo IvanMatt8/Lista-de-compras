1a9f9936af3eeb996afa976b0a3ef3f0aa509716
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-21 16:14:12 -0300

    :pencil: adicionando funcionalidades para o AddArea

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,10 +1,38 @@
+import { useState, KeyboardEvent } from "react";
+
 import * as C from "./styles";
 
-export const AddArea = () => {
+type Props = {
+  onEnter: (taskName: string) => void;
+};
+
+export const AddArea = ({ onEnter }: Props) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.code === "Enter" && inputText !== "") {
+      onEnter(inputText);
+      setInputText("");
+    }
+  };
+
+  const handleClick = () => {
+    onEnter(inputText);
+    setInputText("");
+  };
+
   return (
     <C.Container>
-      <div className="Image">➕</div>
-      <input type="text" placeholder="Adicione uma nova Compra" />
+      <div className="Image">
+        <button onClick={(e) => handleClick()}>➕</button>
+      </div>
+      <input
+        type="text"
+        placeholder="Adicione uma nova Compra"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
     </C.Container>
   );
 };
