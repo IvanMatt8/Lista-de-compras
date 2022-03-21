babc7b70876186f76f5013f3a5c793b9cc0ab934
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-21 11:24:17 -0300

    :hammer: removendo estrutura padrão do React e acrescentando os components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/item";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Pão com batata", done: false },
+    { id: 2, name: "Presunto e Queijo", done: false },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Compras </C.Header>
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
