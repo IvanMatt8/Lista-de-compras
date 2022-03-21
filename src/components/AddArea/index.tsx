7db059ec36ed272ea52e9a1270164db1a3685f5e
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-21 14:32:53 -0300

    :sparkles: adicionado novo component AddArea.

---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,10 @@
+import * as C from "./styles";
+
+export const AddArea = () => {
+  return (
+    <C.Container>
+      <div className="Image">➕</div>
+      <input type="text" placeholder="Adicione uma nova Compra" />
+    </C.Container>
+  );
+};
